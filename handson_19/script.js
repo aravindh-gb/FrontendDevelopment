7117613bb41789221ea1e0010d2b872e1f05f955
aravindh-gb <166371665+aravindh-gb@users.noreply.github.com>
2026-06-24 22:25:47 +0530

    Calculator Project(24 June 2026)

---
+++ handson_19/script.js
@@ -0,0 +1,13 @@
+function addNumbers(){
+
+    let num1 =
+        Number(document.getElementById("num1").value);
+
+    let num2 =
+        Number(document.getElementById("num2").value);
+
+    let result = num1 + num2;
+
+    document.getElementById("result").innerHTML =
+        "Result: " + result;
+}
